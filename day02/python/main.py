b02386e7f8708b28f726d649fd80b66413e95a53
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-12-03 11:09:19 +0100

    Added day02 stage2 in Python3.

--- day02/python/main.py
+++ day02/python/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-def stage1(program):
-    program[1] = 12
-    program[2] = 2
+def run_program(program):
     rip = 0
 
     while True:
@@ -26,9 +24,30 @@ def stage1(program):
 
     raise Exception("Didn't read the '99' opcode.")
 
+def stage1(program):
+    program[1] = 12
+    program[2] = 2
+    return run_program(program)
+
+def stage2(program):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            p = program.copy()
+            p[1] = noun
+            p[2] = verb
+            try:
+                result = run_program(p)
+                if result == 19690720:
+                    return 100 * noun + verb
+            except:
+                continue
+    raise Exception("No combination produced '19690720'.")
+
+
 
 with open('input.txt', 'r') as f:
     program = [int(number) for number in f.read().split(',')]
 
 
 print('[Stage1] Position 0 value: %d' % stage1(program.copy()))
+print('[Stage2] Position 0 value: %d' % stage2(program.copy()))
