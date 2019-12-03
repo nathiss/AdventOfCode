4a67730130a5ef26c9ae13cf776e73558476b8ce
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-12-03 09:27:11 +0100

    Added stage2 in C++ for day01.

--- day01/cpp/main.cpp
+++ day01/cpp/main.cpp
@@ -13,8 +13,20 @@ int main() {
   std::size_t sum{};
   int module_mass{};
 
+  auto get_fuel = [](auto mass) {
+    return (mass / 3) - 2;
+  };
+
   while (input >> module_mass) {
-    sum += (module_mass / 3) - 2;
+
+    int fuel = module_mass;
+    for (;;) {
+      fuel = get_fuel(fuel);
+      if (fuel <= 0)
+        break;
+      sum += fuel;
+    }
+
   }
 
   std::cout << "Sum of the fuel requirements: " << sum << std::endl;
