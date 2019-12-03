2fea647105aaa572d110fc99d43af9374fd1162d
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-12-03 09:33:29 +0100

    Added stage2 in Python3 for day01.

--- day01/python/main.py
+++ day01/python/main.py
@@ -3,6 +3,17 @@
 with open('input.txt', 'r') as f:
     input = [int(mass) for mass in f.read().splitlines()]
 
-sum = sum([(mass // 3) - 2 for mass in input])
 
-print('Sum of the fuel requirements: %d' % sum)
+stage1_sum = sum([(mass // 3) - 2 for mass in input], 0)
+print('[Stage1] Sum of the fuel requirements: %d' % stage1_sum)
+
+
+stage2_sum = 0
+for fuel in input:
+    while True:
+        fuel = (fuel // 3) - 2
+        if fuel <- 0:
+            break
+        stage2_sum += fuel
+
+print('[Stage2] Sum of the fuel requirements: %d' % stage2_sum)
