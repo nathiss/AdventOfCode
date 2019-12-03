2e31e419cd66cf5587ce7e167692927a48cbf299
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-12-03 13:13:31 +0100

    Changed the indent from 4 to 2.

--- day01/python/main.py
+++ day01/python/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 with open('input.txt', 'r') as f:
-    input = [int(mass) for mass in f.read().splitlines()]
+  input = [int(mass) for mass in f.read().splitlines()]
 
 
 stage1_sum = sum([(mass // 3) - 2 for mass in input], 0)
@@ -10,10 +10,10 @@ print('[Stage1] Sum of the fuel requirements: %d' % stage1_sum)
 
 stage2_sum = 0
 for fuel in input:
-    while True:
-        fuel = (fuel // 3) - 2
-        if fuel <- 0:
-            break
-        stage2_sum += fuel
+  while True:
+    fuel = (fuel // 3) - 2
+    if fuel <- 0:
+      break
+    stage2_sum += fuel
 
 print('[Stage2] Sum of the fuel requirements: %d' % stage2_sum)
--- day02/python/main.py
+++ day02/python/main.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
 
 def run_program(program):
-    rip = 0
+  rip = 0
 
-    while True:
-        opcode = program[rip]
-        if opcode == 1:
-            value_1 = program[program[rip + 1]]
-            value_2 = program[program[rip + 2]]
-            result_idx = program[rip + 3]
-            program[result_idx] = value_1 + value_2
-            rip += 4
-        elif opcode == 2:
-            value_1 = program[program[rip + 1]]
-            value_2 = program[program[rip + 2]]
-            result_idx = program[rip + 3]
-            program[result_idx] = value_1 * value_2
-            rip += 4
-        elif opcode == 99:
-            return program[0]
-        else:
-            raise ValueError("Unknown opcode %d" % opcode)
+  while True:
+      opcode = program[rip]
+      if opcode == 1:
+        value_1 = program[program[rip + 1]]
+        value_2 = program[program[rip + 2]]
+        result_idx = program[rip + 3]
+        program[result_idx] = value_1 + value_2
+        rip += 4
+      elif opcode == 2:
+        value_1 = program[program[rip + 1]]
+        value_2 = program[program[rip + 2]]
+        result_idx = program[rip + 3]
+        program[result_idx] = value_1 * value_2
+        rip += 4
+      elif opcode == 99:
+        return program[0]
+      else:
+        raise ValueError("Unknown opcode %d" % opcode)
 
-    raise Exception("Didn't read the '99' opcode.")
+  raise Exception("Didn't read the '99' opcode.")
 
 def stage1(program):
-    program[1] = 12
-    program[2] = 2
-    return run_program(program)
+  program[1] = 12
+  program[2] = 2
+  return run_program(program)
 
 def stage2(program):
-    for noun in range(0, 100):
-        for verb in range(0, 100):
-            p = program.copy()
-            p[1] = noun
-            p[2] = verb
-            try:
-                result = run_program(p)
-                if result == 19690720:
-                    return 100 * noun + verb
-            except:
-                continue
-    raise Exception("No combination produced '19690720'.")
+  for noun in range(0, 100):
+    for verb in range(0, 100):
+      p = program.copy()
+      p[1] = noun
+      p[2] = verb
+      try:
+        result = run_program(p)
+        if result == 19690720:
+          return 100 * noun + verb
+      except:
+        continue
+  raise Exception("No combination produced '19690720'.")
 
 
 
 with open('input.txt', 'r') as f:
-    program = [int(number) for number in f.read().split(',')]
+  program = [int(number) for number in f.read().split(',')]
 
 
 print('[Stage1] Position 0 value: %d' % stage1(program.copy()))
