8ce251145b5fb698105ab59bdb120f2ac86835dc
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2019-12-08 09:49:06 +0100

    Added 2019/day08 stage2 in C++.

--- 2019/day08/cpp/main.cpp
+++ 2019/day08/cpp/main.cpp
@@ -11,8 +11,8 @@
 #include <utility>
 #include <vector>
 
-constexpr int kWidth = 25;
-constexpr int kHeight = 6;
+constexpr std::size_t kWidth = 25;
+constexpr std::size_t kHeight = 6;
 
 struct Layer {
   template <typename It>
@@ -39,6 +39,35 @@ struct Layer {
 
   std::array<std::array<std::uint8_t, kWidth>, kHeight> data;
 };
+
+std::ostream& operator<<(std::ostream& os, const Layer& layer) {
+  for (const auto& row : layer.data) {
+    for (const auto pixel : row) {
+      if (pixel == 0)
+        os << "\033[021;40m ";
+      else
+        os << "\033[021;47m ";
+    }
+    os << "\033[0m\n";
+  }
+  return os;
+}
+
+Layer render_image(const std::vector<Layer>& image) {
+  Layer l;
+  auto get_pixel = [&image](auto h, auto w) -> std::uint8_t {
+    for (const auto& layer : image) {
+      if (const auto pixel = layer.data[h][w]; pixel != 2) {
+        return pixel;
+      }
+    }
+    throw std::runtime_error{"All pixels are transparent."};
+  };
+  for (std::size_t j{}; j < kHeight; j++)
+    for (std::size_t i{}; i < kWidth; i++)
+      l.data[j][i] = get_pixel(j, i);
+  return l;
+}
 
 int main() {
   std::ifstream input_file{"./input.txt"};
@@ -67,6 +96,8 @@ int main() {
 
   std::cout << "[Stage1] Number of `1` digits multiplied by number of `2`: "
     << lowest->first->count(1) * lowest->first->count(2) << std::endl;
+
+  std::cout << "[Stage2]:\n\n" << render_image(image) << std::endl;
 
   return 0;
 }
