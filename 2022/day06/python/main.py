9a0983ab64ce5c3f2ba7b8c6cdb7f3981c0797d8
nathiss <kamil.jakub.rusin@gmail.com>
2022-12-17 21:03:01 +0100

    Make it general for both stages

--- 2022/day06/python/main.py
+++ 2022/day06/python/main.py
@@ -1,15 +1,20 @@
-CHUNK_SIZE = 14
-OVERLAP_WINDOW = CHUNK_SIZE - 1
+def get_windows(collection: str, window_size: int) -> list[str]:
+    return [collection[i:i+window_size] for i in range(0, len(collection))]
 
 if __name__ == "__main__":
     with open("input.txt", "r" ) as f:
         d = f.read().strip()
 
-    windows = [d[i:i+CHUNK_SIZE] for i in range(0, len(d), CHUNK_SIZE-OVERLAP_WINDOW)]
-
-    for window in windows:
+    for window in get_windows(d, 4):
         window_set = set(window)
-        if len(window_set) == CHUNK_SIZE:
+        if len(window_set) == 4:
             marker = ''.join(window)
-            print(f"Stage 1: {d.find(marker)+CHUNK_SIZE}")
+            print(f"Stage 1: {d.find(marker)+4}")
+            break
+
+    for window in get_windows(d, 14):
+        window_set = set(window)
+        if len(window_set) == 14:
+            marker = ''.join(window)
+            print(f"Stage 2: {d.find(marker)+14}")
             break
